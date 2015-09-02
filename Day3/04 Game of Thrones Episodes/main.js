70e192054c2f78da128858e20dd04badd3a2475e
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-09-02 19:08:19 +0200

    finished got exercise

--- Day3/04 Game of Thrones Episodes/main.js
+++ Day3/04 Game of Thrones Episodes/main.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
-var fileActions = require('./fileactions')
+// var fileActions = require('./fileactions')
+var fileActionsRating = require('./fileactions_rating')
 
-fs.readFile("./GoTEpisodes.json", 'utf8', fileActions);
+// fs.readFile("./GoTEpisodes.json", 'utf8', fileActions);
+fs.readFile("./GoTEpisodes.json", 'utf8', fileActionsRating);
