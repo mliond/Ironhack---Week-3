c2f9bcc527bcd7bd7efa251aa60dc695c17a895f
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-09-02 12:25:11 +0200

    Number stats finished

--- Day3/01 First JS/first-exercise.js
+++ Day3/01 First JS/first-exercise.js
@@ -1,21 +1,29 @@
 var numbers = '80:70:90:100';
 var numbers_split = numbers.split(":");
 
-function calculateStats(numbers) {
+function calculateSum(numbers) {
 	var sum = 0;
 	numbers.forEach(function (x) {
 		x = parseInt(x)
 		sum += x;
 	})
+	return sum;
+}
 
+function getHighestNumber(numbers) {
+	var filtered = Math.max.apply( Math, numbers);
+	return filtered;
+}
+
+function giveObject(numbers) {
+	var sum = calculateSum(numbers);
 	var stats = {
 		sum: sum,
-		average: sum/numbers.length
+		average: sum/numbers.length,
+		highest_number: getHighestNumber(numbers)
 	};
-
 	return stats;
 }
 
-
-console.log(calculateStats(numbers_split));
+console.log(giveObject(numbers_split));
 // average(numbers) = 85
