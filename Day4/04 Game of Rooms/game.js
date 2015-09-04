970c7711333aed895d78b8f754e9c5005108abc2
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-09-04 10:00:07 +0200

    finished refactoring

--- Day4/04 Game of Rooms/game.js
+++ Day4/04 Game of Rooms/game.js
@@ -6,10 +6,12 @@ var Game = function () {
   this.user_inventory = [];
 }
 
+// This adds a room to the map array
 Game.prototype.add_room = function (room) {
   this.map.push(room);
 }
 
+// This prints the current room's name and - if existent - inventory.
 Game.prototype.describe_location = function (number) {
   console.log("You are in a " + this.map[number].name);
   if (this.map[number].inventory != "") {
@@ -17,25 +19,27 @@ Game.prototype.describe_location = function (number) {
   }
 };
 
+// This prompts the user for input and moves on the the next function
 Game.prototype.prompt_user = function () {
   var self = this; // because you cant call this in a function
   this.describe_location(this.current_location);
   read({prompt: "Direction: "}, function (err, direction){
-    self.evaluate_input(err, direction);
+    self.command_or_finish_game(err, direction);
   });
 }
 
-Game.prototype.evaluate_input = function (err, direction){
+// This prints the user's command, starts the evaluation, moves back to the function above
+Game.prototype.command_or_finish_game = function (err, direction){
   var self = this; // because you cant call this in a function
   console.log("chose: " + direction);
-  direction = direction.toUpperCase();
+  direction = direction.toUpperCase(); // everything is done in uppercase except dropping inventory
   if (direction != "F") {
-    // self.move_user(direction)
     self.input_direction_or_inventory(direction);
     self.prompt_user();
   };
 };
 
+// Evaluates which function below to move on with
 Game.prototype.input_direction_or_inventory = function (direction) {
   if (direction.indexOf("PICK") > -1 || direction.indexOf("DROP") > -1) {
     this.inventory_action(direction);
@@ -44,6 +48,7 @@ Game.prototype.input_direction_or_inventory = function (direction) {
   };
 }
 
+// Inventory pick and drop function
 Game.prototype.inventory_action = function(direction) {
   if (direction.indexOf("PICK") > -1 && direction.indexOf(this.map[this.current_location].inventory.toUpperCase() > -1)) {
     this.user_inventory.push(this.map[this.current_location].inventory);
@@ -51,17 +56,13 @@ Game.prototype.inventory_action = function(direction) {
     console.log("Your inventory is " + this.user_inventory);
   } else if (direction.indexOf("DROP") > -1 && direction.indexOf(this.user_inventory > -1)) {
     direction = direction.toLowerCase().replace("drop ", "");
-    console.log(direction);
-    var index = this.user_inventory.indexOf(direction);
-    if (index > -1) {
-      this.user_inventory.splice(index, 1);
-    };
-    console.log("Dropped the " + direction);
+    this.user_inventory.splice(this.user_inventory.indexOf(direction), 1);
     console.log("Your inventory is " + this.user_inventory);
     this.map[this.current_location].inventory = direction;
   };
 }
 
+// Move the current_location to another room
 Game.prototype.move_user = function (direction) {
   switch(direction) {
     case "N":
